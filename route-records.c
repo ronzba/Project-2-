599fa3710c7bdaec5315fe7400435ed0d1b3d7d5
ronzba <46931450+ronzba@users.noreply.github.com>
2023-03-23 18:09:15 -0500

    Update route-records.c

--- route-records.c
+++ route-records.c
@@ -10,6 +10,30 @@ RouteRecord* createRecords(FILE* fileIn){
 		return 1;
 	}
 	
+	// Count the records
+	int read = 0;
+	int records = 0;
+	do
+	{
+		read = fscanf(file,
+		            "%d,%3[^,],%3[^,],%2[^,],%10[^,],%d"
+					 &RouteRecord[records].month);
+					 RouteRecord[records].origin);
+					 RouteRecord[records].destination);
+					 RouteRecord[records].airline);
+					 &RouteRecord[records].passengerCount);
+		records++;
+		
+		if (ferror(file)){
+			printf("Error Reading File");
+			return 1;
+		}
+		            
+	} while (!foef(file));
+	
+	//output records read
+	printf("\n Records: ", records);
+	
 	// Allocate Memory for array
 	RouteRecord *routeRecord = malloc(sizeof(RouteRecord) * MAX_FLIGHTS);
 	
@@ -23,3 +47,4 @@ RouteRecord* createRecords(FILE* fileIn){
 	// Rewind File
 	rewind(file);
 }
+// close file at somepoint
