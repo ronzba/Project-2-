ee1763b3e4021a3caf7d1e1bbbacd483c2509a37
ronzba <46931450+ronzba@users.noreply.github.com>
2023-03-31 21:25:20 -0500

    Update route-records.h

--- route-records.h
+++ route-records.h
@@ -18,5 +18,6 @@ int fillRecords(RouteRecord* r, FILE* fileIn);
 int findAirlineRoute(RouteRecord* r, int length, const char* origin, const char* destination, const char* airline, int curIdx);
 void searchRecords(RouteRecord* r, int length, const char* key1, const char* key2, SearchType st);
 void printMenu();
+int countUniqueRoutes(RouteRecord* records, int count);
 
 #endif
