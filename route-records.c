a67efcceb93a6a67908c8b0bff265a4b1341d204
ronzba <46931450+ronzba@users.noreply.github.com>
2023-04-06 03:41:49 -0500

    Update route-records.c

--- route-records.c
+++ route-records.c
@@ -3,85 +3,98 @@
 #include <string.h>
 #include "route-record.h"
 
-#define MAX_RECORDS 8000
 
-RouteRecord* createRecords(FILE* fileIn) {
+RouteRecord* createRecords(FILE* fp) {
+	
+	RouteRecord data;
     char line[100];
     int count = 0;
+    
     // Count number of records in the file
-    while (fgets(line, 100, fileIn)) {
-        if (count != 0) { // ignore header
-            count++;
-        }
+    while (fgets(line, 100, fp) != NULL)
+	{    
+        ++count;
     }
+    
+    count = count - 1;
+    
     // Allocate memory for array of RouteRecord structs
     RouteRecord* records = malloc(count * sizeof(RouteRecord));
+    
     // Initialize passenger count to 0 for each month
-    int i,j;
+   int i,j;
     
     for (i = 0; i < count; i++) {
-        for (j = 0; j < 6; j++) {
-            records[i].passengerCount[j] = 0;
+       for (j = 0; j < 6; j++) {
+           records[i].passengerCount[j] = 0;
         }
     }
     // Rewind file pointer
-    rewind(fileIn);
+   rewind(fp);
     // Return pointer to array of RouteRecord structs
     return records;
 }
 
-int fillRecords(RouteRecord* r, FILE* fileIn) {
+int fillRecords(RouteRecord* records, FILE* fp){
     char line[100];
     int count = 0;
-    // Process each line in the file
-    while (fgets(line, 100, fileIn)) {
-        if (count != 0) { // ignore header
-            // Parse CSV line
-            char* origin = strtok(line, ",");
-            char* destination = strtok(NULL, ",");
-            char* airline = strtok(NULL, ",");
-            int passengers[6];
+
+    // Process each line
+    while (fgets(line, 100, fp)){
+        if (count != 0){
+            sscanf(line, "%*d, %3[^,], %3[^,], %2[^,], %*[^,], %d, %d, %d, %d, %d, %d, %d", records->origin, records->destination, records->airline,
+           		&records->passengerCount[0], &records->passengerCount[1],
+				&records->passengerCount[2], &records->passengerCount[3],
+           		&records->passengerCount[4], &records->passengerCount[5]);
+        }
+        // Find existing record with same origin, destination, and airline
+        int idx = findAirlineRoute(records, count, records->origin, records->destination, records->airline, 0);
+        if (idx != -1) { // update existing record
             int i;
             for (i = 0; i < 6; i++) {
-                passengers[i] = atoi(strtok(NULL, ","));
+                records[idx].passengerCount[i] += records->passengerCount[i];
             }
-            // Find existing record with same origin, destination, and airline
-            int idx = findAirlineRoute(r, count, origin, destination, airline, 0);
-            if (idx != -1) { // update existing record
+        } else { // add new record
+            strncpy(records[count].origin, records->origin, 3);
+            strncpy(records[count].destination, records->destination, 3);
+            strncpy(records[count].airline, records->airline, 2);
             int i;
-                for (i = 0; i < 6; i++) {
-                    r[idx].passengerCount[i] += passengers[i];
-                }
-            } else { // add new record
-                strncpy(r[count].origin, origin, 3);
-                strncpy(r[count].destination, destination, 3);
-                strncpy(r[count].airline, airline, 3);
-                int i;
-                for (i = 0; i < 6; i++) {
-                    r[count].passengerCount[i] = passengers[i];
-                }
-                count++;
+            for (i = 0; i < 6; i++) {
+                records[count].passengerCount[i] = records->passengerCount[i];
             }
+            count++;
         }
     }
     // Return actual number of records used in array
     return count;
 }
 
-int findAirlineRoute(RouteRecord* r, int length, const char* origin, const char* destination, const char* airline, int curIdx) {
-    if (curIdx >= length) { // base case: record not found
-        return -1;
-    }
-    if (strncmp(r[curIdx].origin, origin, 3) == 0 &&
-        strncmp(r[curIdx].destination, destination, 3) == 0 &&
-        strncmp(r[curIdx].airline, airline, 3) == 0) { // base case: record found
-        return curIdx;
-    }
-    // Recursive case: check next record
-    return findAirlineRoute(r, length, origin, destination, airline, curIdx + 1);
+
+int findAirlineRoute(RouteRecord* records, int length, const char* origin, const char* destination, const char* airline, int curIdx){
+	if (curIdx >= length){ // basecase
+		return -1;
+	}
+	if (strncmp(records[curIdx].origin, origin, 3) == 0 && strncmp(records[curIdx].destination, destination, 3)== 0 && strncmp(records[curIdx].airline, airline,2) == 0)
+	{
+		return curIdx;
+	}
+	
+	// Recursive Case
+	return findAirlineRoute(records, length, origin, destination, airline, curIdx + 1);
 }
 
-void searchRecords(RouteRecord* r, int length, const char* key1, const char* key2, SearchType st) {
+void printMenu()
+{
+printf( "\n\n######### Airline Route Records Database MENU #########\n" );
+printf( "1. Search by Route\n" );
+printf( "2. Search by Origin Airport\n" );
+printf( "3. Search by Destination Airport\n" );
+printf( "4. Search by Airline\n" );
+printf( "5. Quit\n" );
+printf( "Enter your selection: " );
+}
+
+void searchRecords(RouteRecord* records, int length, const char* key1, const char* key2, SearchType st) {
     int found = 0;
     int totalPassengers = 0;
     int totalPassengersByMonth[6] = { 0 };
@@ -89,7 +102,7 @@ void searchRecords(RouteRecord* r, int length, const char* key1, const char* key
     
     int i;
     for (i = 0; i < length; i++) {
-        RouteRecord record = r[i];
+        RouteRecord record = records[i];
         int matchesKey = 0;
         
         switch (st) {
@@ -141,34 +154,3 @@ void searchRecords(RouteRecord* r, int length, const char* key1, const char* key
         printf("No matching records found.\n");
     }
 }
-
-void printMenu()
-{
-	printf( "\n\n######### Airline Route Records Database MENU #########\n" );
-	printf( "1. Search by Route\n" );
-	printf( "2. Search by Origin Airport\n" );
-	printf( "3. Search by Destination Airport\n" );
-	printf( "4. Search by Airline\n" );
-	printf( "5. Quit\n" );
-	printf( "Enter your selection: " );
-}
-
-int countUniqueRoutes(RouteRecord* records, int count) {
-    int uniqueCount = 0;
-    int i;
-    for (i = 0; i < count; i++) {
-        int j;
-        for (j = 0; j < i; j++) {
-            if (strcmp(records[i].origin, records[j].origin) == 0 &&
-                strcmp(records[i].destination, records[j].destination) == 0) {
-                break;  // found a duplicate
-            }
-        }
-        if (j == i) {  // no duplicates found
-            uniqueCount++;
-        }
-    }
-    return uniqueCount;
-}
-
-
