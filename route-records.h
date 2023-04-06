f0f676120bb4689999e954070bc32ea2eea18a38
ronzba <46931450+ronzba@users.noreply.github.com>
2023-04-06 03:42:07 -0500

    Update route-records.h

--- route-records.h
+++ route-records.h
@@ -3,21 +3,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define MONTHS 6
 
 typedef enum SearchType { ROUTE, ORIGIN, DESTINATION, AIRLINE } SearchType;
 
 typedef struct RouteRecord {
     char origin[4];
     char destination[4];
-    char airline[4];
+    char airline[3];
     int passengerCount[6];
 } RouteRecord;
 
-RouteRecord* createRecords(FILE* fileIn);
-int fillRecords(RouteRecord* r, FILE* fileIn);
-int findAirlineRoute(RouteRecord* r, int length, const char* origin, const char* destination, const char* airline, int curIdx);
+RouteRecord* createRecords(FILE* fp);
+int fillRecords(RouteRecord* , FILE* fp);
+int findAirlineRoute(RouteRecord* records, int length, const char* origin, const char* destination, const char* airline, int curIdx);
 void searchRecords(RouteRecord* r, int length, const char* key1, const char* key2, SearchType st);
 void printMenu();
-int countUniqueRoutes(RouteRecord* records, int count);
+
 
 #endif
